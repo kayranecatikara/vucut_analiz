98317a53fc687a6636e5fda844c96ea3c897598a
kayranecatikara <60383469+kayranecatikara@users.noreply.github.com>
2025-07-10 05:18:22 +0300

    Kalori analizinde RGB görüntü kullan

--- backend/app_test_system.py
+++ backend/app_test_system.py
@@ -258,7 +258,6 @@ def take_food_photo_realsense():
     
     try:
         realsense_pipeline = rs.pipeline()
-        config = rs.config()
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
         
         profile = realsense_pipeline.start(config)
